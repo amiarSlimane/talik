8f0ad3ab66883f96005ad14d5384c837a777139c
amiar <amiarslimane@yandex.ru>
2022-09-09 08:00:25 +0100

    init model

---
+++ comments-service/server/models/postModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+ 
+// User
+const postSchema = new mongoose.Schema({
+    host: {
+        type: String,
+    },
+    user:{
+        type:mongoose.Types.ObjectId
+    }
+},{
+    timestamps:true
+});
+
+postSchema.pre('save', function (next) {
+    
+    next();
+});
+
+postSchema.pre(/^find/, function (next) {
+    next();
+});
+
+const Post = mongoose.model('post', postSchema)
+
+module.exports = Post;
